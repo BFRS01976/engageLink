3db0f23534694b89da26e68b65b26f5334fe1c7b
Sonia <sonia1@shiprocket.com>
2024-11-16 23:48:11 +0530

    -

--- services/firebase.service.js
+++ services/firebase.service.js
@@ -42,7 +42,7 @@ const setupFirebase = async (registration) => {
   // messaging.useServiceWorker('./dist/firebase-messaging-sw.js');
   if ('serviceWorker' in navigator) {
     //   window.addEventListener('load', () => {
-    navigator.serviceWorker.register('/dist/firebase-messaging-sw.js', { scope: '/dist/' })
+    navigator.serviceWorker.register('/dist/firebase-messaging-sw.js', {  })
       .then((registration) => {
 
         console.log('Service Worker registered with scope:', registration.scope);
